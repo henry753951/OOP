aca1939773a72039d03279d62add1fe3962de621
henry <henryboy753951@gmail.com>
2022-12-17 14:07:17 +0800

    init

---
+++ src/game.cpp
@@ -0,0 +1,20 @@
+
+
+#pragma once  // 防止重複引入
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <iostream>
+
+class Game {
+   private:
+    /* data */
+   public:
+    Game(/* args */);
+    ~Game();
+};
+
+Game::Game(/* args */) {}
+
+Game::~Game() {}
