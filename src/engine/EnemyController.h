83150894d53684d75d146bd07e13ee8661600b27
henry <henryboy753951@gmail.com>
2023-01-12 23:42:57 +0800

    hi

--- src/engine/EnemyController.h
+++ src/engine/EnemyController.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include "../EnemyStatComponent.h"
 #include "../header/Game.h"
 #include "Components.h"
 #include "ECS/ECS.h"
 #include "SpriteComponent.h"
 #include "TransformComponent.h"
-#include "../EnemyStatComponent.h"
 
 extern Manager manager;
 using std::cin;
@@ -13,19 +13,17 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-class EnemyController : public Component
-{
-private:
+class EnemyController : public Component {
+   private:
     vector<Entity *> players;
     float distanceX, distanceY;
 
-public:
+   public:
     EnemyStatComponent *stat;
     TransformComponent *transform;
     SpriteComponent *sprite;
 
-    void init() override
-    {
+    void init() override {
         players = manager.getGroup(Game::groupPlayers);
         transform = &entity->getComponent<TransformComponent>();
         stat = &entity->getComponent<EnemyStatComponent>();
@@ -33,8 +31,7 @@ public:
     }
 
     void update() override {
-               for (auto &p : players)
-        {
+        for (auto &p : players) {
             distanceX = (p->getComponent<TransformComponent>().position.x) - transform->position.x;
             distanceY = (p->getComponent<TransformComponent>().position.y) - transform->position.y;
             // cout << distanceX << " ";
@@ -44,28 +41,26 @@ public:
         sprite->angle = -10 + (atan2(distanceY, distanceX) * 180.0000) / M_PI;
         // cout << sprite->angle << endl;
 
-                       // 面向滑鼠
-                       //  if (sprite->destRect.x + 32 < mousePosX) {
-                       //      sprite->spriteFlip = SDL_FLIP_NONE;
-                       //  } else if (sprite->destRect.x + 32 > mousePosX) {
-                       //      sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
-                       //  }
+        // 面向滑鼠
+        //  if (sprite->destRect.x + 32 < mousePosX) {
+        //      sprite->spriteFlip = SDL_FLIP_NONE;
+        //  } else if (sprite->destRect.x + 32 > mousePosX) {
+        //      sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
+        //  }
 
-                       //  動畫
+        //  動畫
         if (transform->velocity.y == 0 && transform->velocity.x == 0) {
             sprite->Play("pistol_idle");
-        }
-        else
-        {
+        } else {
             sprite->Play("pistol_walk");
         }
 
-        if(stat->nowMode == 0  && distanceX == 0 && distanceY == 0){
+        if (stat->nowMode == 0 && distanceX == 0 && distanceY == 0) {
             transform->velocity.x = 0;
             transform->velocity.y = 0;
-        }else if(stat->nowMode == 0){
-            transform->velocity.x =  stat->speed * (distanceX / sqrt(pow(distanceX,2)+pow(distanceY,2)));
-            transform->velocity.y =  stat->speed * (distanceY / sqrt(pow(distanceX,2)+pow(distanceY,2)));
+        } else if (stat->nowMode == 0) {
+            transform->velocity.x = stat->speed * (distanceX / sqrt(pow(distanceX, 2) + pow(distanceY, 2)));
+            transform->velocity.y = stat->speed * (distanceY / sqrt(pow(distanceX, 2) + pow(distanceY, 2)));
         }
 
         // if (Game::event.type == SDL_KEYUP) {
